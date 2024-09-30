eb8932ed65aceef437bc638cd4ca4255d5fe45c4
Nouredine Tahrioui <nouredine.tahrioui@bazandpoort.be>
2024-09-30 13:41:10 +0200

    Added database into code

---
+++ footwear-master/db.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "footwear_db");
+
+$aanpassenUTF = $mysqli->query("SET NAMES utf8");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+?>
